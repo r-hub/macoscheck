b9462271d6ba40a70b5b47149b962cfb5affd0c0
Gábor Csárdi <csardi.gabor@gmail.com>
2019-05-30 10:52:57 +0100

    Use Biocmanager instead of bioclite.R / BiocInstaller
    https://github.com/r-hub/devops-issues/issues/43

--- slave.sh
+++ slave.sh
@@ -65,9 +65,9 @@ setup_r_environment() {
     echo >> .Rprofile '.libPaths("~/R")'
 
     # BioC
-    R -q -e "source('https://bioconductor.org/biocLite.R')"
-    echo >> .Rprofile 'options(repos = BiocInstaller::biocinstallRepos())'
-    echo >> .Rprofile 'unloadNamespace("BiocInstaller")'
+    R -q -e "install.packages('BiocManager')"
+    echo >> .Rprofile 'options(repos = BiocManager::repositories())'
+    echo >> .Rprofile 'unloadNamespace("BiocManager")'
 
     # locales
     export LC_ALL=en_US.UTF-8
