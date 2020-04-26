47277645b7a8e96df49b845e88e513eeba10d260
R-hub admin <rhub@R-hubs-Mac-2.local>
2020-04-26 03:18:06 -0700

    Refine dependencies
    Also need TeX...

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ else
 fi
 
 function install_brew() {
+    echo "== INSTALLING BREW ================================"
     (
 	set -e
 	if /usr//bin/which -s brew; then return; fi
@@ -24,27 +25,78 @@ function install_brew() {
 }
 
 function update_sysreqs() {
+    echo "== UPDATING SYSREQS ==============================="
     (
 	set -e
-	echo "== UPDATING BREW ======================"
-	brew update
-
-	brew cask install homebrew/cask-versions/adoptopenjdk8
 	forms=$(cat sysreqs.txt)
-	for f in $forms; do
-	    echo "-- INSTALLING $f --"
-	    brew install $f
-	    brew upgrade $f
-	done
+	brew install $forms
+    )
+}
+
+function install_xquartz() {
+    echo "== INSTALLING XQUARTZ ============================="
+    (
+	set -e
+	if (pkgutil --pkgs | grep -q org.macosforge.xquartz.pkg); then
+	    return;
+	fi
+	local version=2.7.11
+	curl -C - -L -O -f \
+	    https://dl.bintray.com/xquartz/downloads/XQuartz-${version}.dmg
+	sudo hdiutil attach XQuartz-${version}.dmg
+	sudo installer -package /Volumes/XQuartz-${version}/XQuartz.pkg \
+	    -target /
+	sudo hdiutil detach /Volumes/XQuartz-${version}
     )
 }
 
 function install_r() {
-    true
+    echo "== INSTALLING R ${1} =============================="
+    (
+	set -e
+	local version="$1"
+	url="https://cloud.r-project.org/bin/macosx/R-${version}.pkg"
+	ur2="https://cloud.r-project.org/bin/macosx/old/R-${version}.pkg"
+	curl -C - -O -f "$url" || curl -C - -O -f "$ur2"
+	sudo installer -pkg "R-${version}.pkg" -target /
+    )
+}
+
+function install_java() {
+    echo "== INSTALLING JAVA ================================"
+    brew cask install java
+}
+
+function install_tex() {
+    echo "== INSTALLING TEX ================================="
+    brew cask install mactex-no-gui
+}
+
+function install_r_hub_client() {
+    curl -O -C - https://repo.jenkins-ci.org/releases/org/jenkins-ci/plugins/swarm-client/2.2/swarm-client-2.2-jar-with-dependencies.jar
+}
+
+function install_gfortran_82() {
+    echo "== INSTALLING GFORTRAN 8.2 ========================"
+    (
+	set -e
+	curl -L -O -f -C - https://github.com/fxcoudert/gfortran-for-macOS/releases/download/8.2/gfortran-8.2-Mojave.dmg
+	sudo hdiutil attach gfortran-8.2-Mojave.dmg
+	sudo installer -package \
+	    /Volumes/gfortran-8.2-Mojave/gfortran-8.2-Mojave/gfortran.pkg \
+	    -target /
+	sudo hdiutil detach /Volumes/gfortran-8.2-Mojave
+    )
 }
 
 function main() {
-    true
+    install_xquartz
+    install_brew
+    update_sysreqs
+    install_java
+    install_r 4.0.0
+    install_gfortran_82
+    install_r_hub_client
 }
 
 if [ "$sourced" = "0" ]; then
